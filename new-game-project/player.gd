9bf148b1cbb89862f781a986ebe12933f64a9b16
Asluenx <asluenx@outlook.com>
2026-02-28 13:59:15 +1000

    bingus

--- new-game-project/player.gd
+++ new-game-project/player.gd
@@ -9,10 +9,13 @@ const maxX = 90
 const minX = -90
 var state = 0
 var interact = 0
+var flashlightOn = 0
 
 @onready var head = $camPos
 @onready var camera = $camPos/Camera3D
 @onready var raycast = $camPos/Camera3D/RayCast3D
+@onready var flashlight = $camPos/Camera3D/SpotLight3D
+@onready var flashlight2 = $camPos/Camera3D/SpotLight3D2
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -40,8 +43,15 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
-	if Input.is_action_just_pressed("Interact") and interact == 1:
-		state = 1
+	if Input.is_action_just_pressed("Interact"):
+		if flashlightOn == 0:
+			flashlight.light_energy = 20
+			flashlight.light_energy = 5
+			flashlightOn = 1
+		elif flashlightOn == 1:
+			flashlight.light_energy = 0
+			flashlight.light_energy = 0
+			flashlightOn = 0
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
